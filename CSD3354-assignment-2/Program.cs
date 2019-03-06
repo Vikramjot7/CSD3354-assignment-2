3f7f9867d8e8744e5a11687f6fd963f85622c493
visualstudio <visualstudio@122-PC31>
2019-03-06 11:32:34 -0500

    lab4

--- CSD3354-assignment-2/Program.cs
+++ CSD3354-assignment-2/Program.cs
@@ -11,30 +11,40 @@ using System.Threading.Tasks;
 
 namespace CSD3354_assignment_2
 {
-   
-            class proram
+
+    public class DelegateExercises
+    {
+        public delegate int MyDelegate(int intValue);
+
+        public int Method1(int intMethod1)
         {
-            static void Main(string[] args)
-            {
-                DelegateExercises a = new DelegateExercises();
-                a.Method2();
-            }
+            return intMethod1 * 2;
         }
-        public class DelegateExercises
+
+        public int Method2(int intMethod2)
         {
-            public delegate void MyDelegate();
+            return intMethod2 * 10;
+        }
 
-            void Method1()
-            {
-                Console.WriteLine("Method1");
-                Console.ReadLine();
-
-            }
-            public void Method2()
-            {
-                MyDelegate myDelegate = new MyDelegate(Method1);
-                myDelegate();
-            }
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            int result1 = myDelegate(10);
+            System.Console.WriteLine(result1);
+            myDelegate = new MyDelegate(Method2);
+            int result2 = myDelegate(10);
+            System.Console.WriteLine(result2);
         }
     }
-  
+
+    public class Program
+    {
+        public static void Main()
+        {
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method3();
+            Console.ReadLine();
+
+        }
+    }
+}
