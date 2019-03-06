33cdc6373ce5ce1744a17819f671c8811f53d562
visualstudio <visualstudio@122-PC31>
2019-03-06 12:02:14 -0500

    lab2

--- CSD3354-assignment-2/Program.cs
+++ CSD3354-assignment-2/Program.cs
@@ -7,12 +7,13 @@ using System;
 //Assignment 2
 //March 6,2019using System;
 
+
 namespace DelegatesAndEvents
 {
     public class DelegateExercises
     {
         public delegate void MyDelegate();
-        void Method1(int i)
+        void Method1()
         {
             Console.WriteLine("Method1");
             Console.ReadLine();
@@ -20,19 +21,8 @@ namespace DelegatesAndEvents
         public void Method2()
         {
             MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
+            myDelegate(50);
         }
 
-    }
-}
-namespace DelegatesAndEvents
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            DelegateExercises delegateExercises = new DelegateExercises();
-            delegateExercises.Method2();
-        }
     }
 }
