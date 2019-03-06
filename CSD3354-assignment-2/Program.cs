3b19a8c9d0156cf066db840a90d7546f9e44deea
visualstudio <visualstudio@122-PC31>
2019-03-06 11:53:13 -0500

    lab11

--- CSD3354-assignment-2/Program.cs
+++ CSD3354-assignment-2/Program.cs
@@ -1,46 +1,17 @@
 ﻿
 
-namespace DelegatesAndEvents
+public delegate void MyDelegate();
 //Student Name Jashanpreet Singh Student ID C0732125
 //Student Name Vikramjot Singh Student ID C0726863
 //CSD3354 Section 2
 //Assignment 2
 //March 6,2019using System;
 
+
+public class DelegateExercises
 {
-    using System;
-
-    delegate void ExampleDelegate(string xyz);
-
-    class Program
+    void Method3()
     {
-        public static void Method1(string xyz)
-        {
-            Console.WriteLine(xyz + " Method1");
-        }
-
-        public static void Method2(string xyz)
-        {
-            Console.WriteLine(xyz + " Method2");
-        }
-
-        public static void Main()
-        {
-            ExampleDelegate ex1Delegate, ex2Delegate, ex3Delegate, myDelegate;
-
-            ex1Delegate = new ExampleDelegate(Method1);
-            ex2Delegate = new ExampleDelegate(Method2);
-            ex3Delegate = ex1Delegate + ex2Delegate;
-            myDelegate = ex1Delegate - ex2Delegate;
-            ex1Delegate("AAA");
-            ex2Delegate("BBB");
-            ex3Delegate("CCC");
-            myDelegate("DDD");
-            myDelegate = ex3Delegate - ex1Delegate;
-            myDelegate("EEE");
-            myDelegate = ex3Delegate - ex2Delegate;
-            myDelegate("FFF");
-            Console.ReadLine();
-        }
+        System.Console.WriteLine(MyDelegate.ToString());
     }
 }
